1af7e534cba0d4fcb89fb233ee475c791d5dbf83
Wiltomar DUARTE <wiltomar@yahoo.com.br>
2020-04-08 15:57:44 -0300

    Commit final do desafio 03.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function getRepositories() {
+      try {
+        const resp = await api.get('/repositories');
+        
+        setRepositories(resp.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const repository = {
+        url: 'https://github.com/wiltomar/desafio03',
+        title: 'Terceiro desafio GoStack - Conhecendo o Reactjs',
+        techs: ['JavaScript', 'Reactjs', 'Node.js']
+      };
+
+      const resp = await api.post('/repositories', repository);
+
+      setRepositories([...repositories, resp.data]);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      setRepositories(repositories.filter((repository) => repository.id !== id));
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
